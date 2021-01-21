39bdc3e7c8060ad8a07510938cc5c6e43dbce116
mli9502 <mengwen9411@gmail.com>
2021-01-20 22:13:28 -0500

    hw0 done without auth.

--- hw0/index.js
+++ hw0/index.js
@@ -20,10 +20,19 @@ const port = process.env.PORT || "9000";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(exp.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     // res.status(200).send("Hello World!");
     res.render("index", {title: "Home!"});
+});
+app.get("/user", (req, res) => {
+    res.render("user", {
+        title: "User!", 
+        userProfile: {
+            nickname: "Test Nickname"
+        }
+    });
 });
 
 /**
